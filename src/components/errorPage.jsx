bf6161874ef01e96517724e54a709702785447e6
melanie mwendwa <mwendwathuranira@gmail.com>
2025-07-17 06:15:25 +0300

    update

---
+++ src/components/errorPage.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+
+function ErrorPage() {
+  return (
+    <>
+      <NavBar />
+      <main>
+        <h1>Oops! Looks like something went wrong.</h1>
+      </main>
+    </>
+  );
+}
+
+export default ErrorPage;
